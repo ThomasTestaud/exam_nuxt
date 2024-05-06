4861eeb2645d25f5714fbf6037d4a27dbb1381f5
ThomasTestaud <thomas.testaud@hotmail.fr>
2024-05-06 16:07:59 +0200

    fix routes

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,8 @@
 <template>
-    <div>
+    Select a page:
         <button v-for="model in models" :key="model.name" @click="selected = model" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             {{ model.name }}
         </button>
-    </div>
-    <Fetcher :model="selected" :amount="2" />
 </template>
 
 <script setup>
